d826f26b94205379420a0887ef905562a198f501
Matt Wytock <mwytock@gmail.com>
2016-08-10 10:05:19 -0700

    fix some bugs and simplify problem_test

--- cvxflow/problem.py
+++ cvxflow/problem.py
@@ -1,59 +1,63 @@
 
 from collections import namedtuple
+from cvxpy.lin_ops import lin_utils
 from cvxpy.lin_ops import tree_mat
 import cvxpy as cvx
-import operator
 import tensorflow as tf
 
 from cvxflow import cvxpy_expr
 from cvxflow.tf_util import vec, mat, vstack
 
-def get_slices(elems, key):
+def get_slices(x_sizes):
     offset = 0
     slices = {}
-    for x in elems:
-        size = x.size[0]*x.size[1]
-        slices[key(x)] = slice(offset, offset+size)
+    for x_id, x_size in x_sizes:
+        size = x_size[0]*x_size[1]
+        slices[x_id] = slice(offset, offset+size)
         offset += size
     return slices
 
-def get_vars(obj, constrs):
+def get_var_sizes(obj, constrs):
     vars_ = lin_utils.get_expr_vars(obj)
     for constr in constrs:
-        vars_ += lin_utils.get_expr_vars(constr)
+        vars_ += lin_utils.get_expr_vars(constr.expr)
     return list(set(vars_))
+
+def get_constr_sizes(constrs):
+    return [(constr.constr_id, constr.size) for constr in constrs]
 
 class TensorProblem(object):
     def __init__(self, cvx_problem):
         obj, constrs = cvx_problem.canonicalize()
 
-        self.vars = get_vars(obj, constrs)
-        self.var_slices = get_slices(self.vars, lambda x: x.data)
-        self.constrs = constrs
-        self.constr_slices = get_slices(self.constrs, lambda x: x.constr_id)
+        self.var_sizes = get_var_sizes(obj, constrs)
+        self.var_slices = get_slices(self.var_sizes)
+        self.constr_sizes = get_constr_sizes(constrs)
+        self.constr_slices = get_slices(self.constr_sizes)
 
-        self.A_exprs = {
-            constr.constr_id: constr.expr
-            for constr in tree_mat.prune_constants(constrs)}
+        # TODO(mwytock): Verify signs on A, b here
+        self.A_exprs = [
+            constr.expr for constr in tree_mat.prune_constants(constrs)]
         self.b = vstack(
-            [vec(tf.constant(tree_mat.mul(constr.expr, {})))
+            [vec(tf.constant(-tree_mat.mul(constr.expr, {})))
              for constr in constrs])
 
-        # get c elements by taking gradient of c'x
-        x = [tf.Variable(tf.zeros(var.size, dtype=tf.float64))]
-        obj = cvxpy_expr.tensor(
-            obj, dict(zip((var.id for var in cvx_problem.variables()))))
-        self.c = vstack([vec(ci) for ci in tf.gradients(obj, x)])
+        # get c elements via gradient of c'x
+        xs = [tf.Variable(tf.zeros(var_size, dtype=tf.float64))
+              for _, var_size in self.var_sizes]
+        obj_t = cvxpy_expr.tensor(
+            obj, dict(zip((var_id for var_id, _ in self.var_sizes), xs)))
+        self.c = vstack([vec(ci) for ci in tf.gradients(obj_t, xs)])
 
     def A(self, x):
-        xs = {var.id: tf.reshape(x[self.var_slices[var_id]], var.size)
-              for var in self.vars}
+        xs = {var_id: mat(x[self.var_slices[var_id],0], var_size)
+              for var_id, var_size in self.var_sizes}
         return vstack([vec(cvxpy_expr.tensor(Ai, xs)) for Ai in self.A_exprs])
 
     def AT(self, y):
-        ys = [mat(y[self.constr_slices[constr.constr_id]], constr.size)
-              for constr in self.constrs]
+        ys = [mat(y[self.constr_slices[constr_id],0], constr_size)
+              for constr_id, constr_size in self.constr_sizes]
         x_map = cvxpy_expr.sum_dicts(
             cvxpy_expr.adjoint_tensor(Ai, ys[i])
             for i, Ai in enumerate(self.A_exprs))
-        return vstack(vec(x_map[var.id]) for var in self.vars)
+        return vstack([vec(x_map[var_id]) for var_id, _ in self.var_sizes])
