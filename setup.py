a0ff5c9604cd64c4c15953580c20c39afa349470
Matt Wytock <mwytock@gmail.com>
2016-10-20 14:34:04 -0700

    add packaging and move tests to cvxflow.tests

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "cvxflow",
+    version = "0.1.1",
+    author = "Matt Wytock and Steven Diamond",
+    url = "http://github.com/cvxgrp/cvxflow",
+    author_email = "mwytock@stanford.edu, diamond@stanford.edu",
+    packages = find_packages(),
+    install_requires=[
+        "cvxpy",
+        "tensorflow",
+    ],
+    test_suite = "cvxflow.tests",
+)
