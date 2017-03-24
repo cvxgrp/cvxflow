87c7fb8d0641a7d869d552e5645c5b9ca0948acc
Matt Wytock <matt@gridmatic.com>
2017-03-24 10:14:16 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cvxflow",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Matt Wytock and Steven Diamond",
     url = "http://github.com/cvxgrp/cvxflow",
     author_email = "mwytock@stanford.edu, diamond@stanford.edu",
