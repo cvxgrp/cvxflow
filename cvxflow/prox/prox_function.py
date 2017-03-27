f689895b99bb979015bd1f5bcc52b8dd33ff6c72
Matt Wytock <matt@gridmatic.com>
2017-03-27 13:32:58 -0700

    refactor, move things around

--- cvxflow/prox/prox_function.py
+++ cvxflow/prox/prox_function.py
@@ -18,7 +18,7 @@ class ProxFunction(object):
   @contextlib.contextmanager
   def _name_scope(self, name=None, values=None):
     """Helper function to standardize op scope."""
-    with ops.name_scope(self._name):
+    with ops.name_scope(self.name):
       with ops.name_scope(name, values=(
           ([] if values is None else values) + self.graph_parents)) as scope:
         yield scope
