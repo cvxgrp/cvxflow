3221186e0e57dfdd1bc2835cbf5f9626c919c055
Matt Wytock <matt@gridmatic.com>
2017-03-27 13:32:01 -0700

    add least squares with constraint

--- cvxflow/prox/least_squares.py
+++ cvxflow/prox/least_squares.py
@@ -1,61 +1,86 @@
 
+from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import linalg_ops
 from tensorflow.python.ops import math_ops
 
+from cvxflow.prox import prox_function
 
-def _get_size_and_type(A, W):
+
+def _get_graph_parents(A, b, C, d, mu):
+  graph_parents = []
   if A is not None:
     graph_parents.extend(A.graph_parents)
-    n = int(A.shape[1])
-    dtype = A.dtype
-    A = A.to_dense()
-  elif W is not None:
-    graph_parents.extend(W.graph_parents)
-    n = int(W.shape[1])
-    dtype = W.dtype
-    W = W.to_dense()
-  else:
-    raise ValueError("Must specify either A or W")
+  if b is not None:
+    graph_parents.append(b)
+  if C is not None:
+    graph_parents.extend(C.graph_parents)
+  if d is not None:
+    graph_parents.append(d)
+  if mu is not None:
+    graph_parents.append(mu)
+  return graph_parents
 
 
-def _get_normal_equation(A, W, mu, b):
-  pass
+def _get_normal_equation(A, mu, b, n, dtype):
+  M = 0
+  h = 0
+  mu = mu if mu is not None else 0
+
+  M = (mu+1)*linalg_ops.eye(n, dtype=dtype)
+  if A is not None:
+    M += A.apply(A.to_dense(), adjoint=True)
+    if b is not None:
+      h = A.apply(b, adjoint=True)
+
+  return M, h
+
 
 class LeastSquares(prox_function.ProxFunction):
   """Least squares.
 
   (1/2)||Ax - b||^2 + (mu/2)||x||^2 + I(Cx = d) + (1/2)||x - v||^2
   """
-  def __init__(self, A=None, b=None, C=None, d=None, W=None, mu=None,
+  def __init__(self, A=None, b=None, C=None, d=None, mu=None, n=None, dtype=None,
                name="LeastSquares"):
-    with ops.name_scope(name, values=[A, b, C, d, W, mu]):
-      n, dtype = _compute_size_and_type(A, W)
-      M, h = _get_normal_equation(A, W, mu, b)
-      self.chol = math_ops.cholesky(M)
+    with ops.name_scope(name, values=[A, b, C, d, mu]):
+      if A is None:
+        if n is None or dtype is None:
+          raise ValueError("Must specify A or both dtype and n")
+        dtype = dtype
+        n = n
+      else:
+        dtype = A.dtype
+        n = int(A.shape[1])
+
+      graph_parents = _get_graph_parents(A, b, C, d, mu)
+      M, self.h = _get_normal_equation(A, mu, b, n, dtype)
+      self.chol = linalg_ops.cholesky(M)
 
       if C:
         self.constrained = True
-        eye = linalg_ops.eye(n, dtype=dtype)
-        F = linalg_ops.cholesky_solve(C.apply(eye, adjoint=True))  # M^{-1}C'
-        self.chol_constraint = math_ops.cholesky(C.apply(self.F))
-        self.F = F
         self.C = C
-        self.h = ...
+        self.d = d
+
+        # CM^{-1}C^T
+        self.chol_constraint = C.apply(
+          linalg_ops.cholesky_solve(
+            self.chol,
+            array_ops.transpose(C.to_dense())))
+
       else:
-        self.h = h
         self.constrained = False
 
       super(LeastSquares, self).__init__(
-        graph_parents=[A, b, C, D, W, mu],
+        graph_parents=graph_parents,
         name=name)
 
-
-    def _call(self, v):
-      if self.constrained:
-        y = linalg_ops.cholesky_solve(
-          self.chol_constraint, self.F.apply(v, adjoint=True))
-        return linalg_ops.cholesky_solve(
-          self.chol, self.h + self.C.apply(y, adjoint=True))
-      else:
-        return linalg_ops.cholesky_solve(self.chol, self.g + v)
+  def _call(self, v):
+    if self.constrained:
+      z = linalg_ops.cholesky_solve(self.chol, self.h + v)
+      y = linalg_ops.cholesky_solve(
+        self.chol_constraint, self.C.apply(z) - self.d)
+      return linalg_ops.cholesky_solve(
+        self.chol, self.h + v - self.C.apply(y, adjoint=True))
+    else:
+      return linalg_ops.cholesky_solve(self.chol, self.h + v)
--- cvxflow/prox/least_squares_test.py
+++ cvxflow/prox/least_squares_test.py
@@ -1,97 +1,70 @@
 
 
-from tensorflow.contrib import linalg
-from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
-from tensorflow.python.ops import math_ops
+from tensorflow.contrib import linalg
 from tensorflow.python.platform import test
+from tensorflow.contrib import linalg
 
-from cvxflow.prox import functions
-
-def np_cholesky_solve(L, b):
-    return np.linalg.solve(L.T, np.linalg.solve(L, b))
-
-def np_solve(A, b, C, d, W, v, mu):
-    # [A'A + W'W + mu*I  C'][x] = [A'b + W'v]
-    # [C                 0 ][y]   [d]
-    I = np.eye(A.shape[1])
-    L1 = np.linalg.cholesky(A.T.dot(A) + W.T.dot(W) + mu*I)
-    F = np_cholesky_solve(L1, C.T)
-    L2 = C.dot(C.dot(F))
-    g = A.T.dot(b) + W.T.dot(v)
-    y = np_cholesky_solve(L2, F.T.dot(g) - d)
-    x = np_cholesky_solve(L1, g - C.T.dot(y))
-    return x
+from cvxflow.prox import least_squares
+from cvxflow.prox import prox_function_testutil
 
 
+class LeastSquaresTest(prox_function_testutil.ProxFunctionTest):
+  def _get_prox_function_for_dtype(
+      self, dtype, A=None, b=None, C=None, d=None, mu=None, n=None):
+    A = (linalg.LinearOperatorMatrix(ops.convert_to_tensor(A, dtype=dtype)) if A
+         else None)
+    b = ops.convert_to_tensor(b, dtype=dtype) if b else None
+    C = (linalg.LinearOperatorMatrix(ops.convert_to_tensor(C, dtype=dtype)) if C
+         else None)
+    d = ops.convert_to_tensor(d, dtype=dtype) if d else None
+    mu = ops.convert_to_tensor(mu, dtype=dtype) if mu else None
+    return least_squares.LeastSquares(
+      A=A, b=b, C=C, d=d, mu=mu, dtype=dtype, n=n)
 
-class LeastSquaresTest(test.TestCase):
-    @property
-    def _dtypes_to_test(self):
-        return [dtypes.float32, dtypes.float64]
+  def testBasic1d(self):
+    self._verify(
+      [[1.],[2.]], [[-0.853448], [2.448276]],
+      A=[[1.,2.],[3.,4.],[5.,6.]],
+      b=[[7.],[8.],[9.]],
+      n=2)
+  def testBasic2d(self):
+    self._verify(
+      [[1.,3.], [2., 4.]],
+      [[-0.853448, -0.629311], [2.448276, 2.310345]],
+      A=[[1.,2.],[3.,4.],[5.,6.]],
+      b=[[7.],[8.],[9.]],
+      n=2)
+  def testBasic1dWithMu(self):
+    self._verify(
+      [[1.],[2.]], [[-0.728593], [2.347778]],
+      A=[[1.,2.],[3.,4.],[5.,6.]],
+      b=[[7.],[8.],[9.]],
+      mu=0.1,
+      n=2)
+  def testConstrained1D(self):
+    self._verify(
+      [[1.],[2.]], [[ 1.07022], [2.16489]],
+      C=[[1.,2.],[3.,4.],[5.,6.]],
+      d=[[7.],[8.],[9.]],
+      n=2)
 
-    def
+# TODO(mwytock): Add some random tests using np_solve()
+#class RandomLeastSquaresTest(prox_function_testutil.ProxFunctionTest):
+  # def np_cholesky_solve(L, b):
+  #   return np.linalg.solve(L.T, np.linalg.solve(L, b))
 
-    def _convert_inputs(self, dtype, kwargs):
-        retval = {}
-        for k, v in kwargs.items():
-            if isinstance(v, tuple):
-                v = tuple(ops.convert_to_tensor(x, dtype=dtype) for x in v)
-            elif isinstance(v, linalg.LinearOperatorMatrix):
-                v = linalg.LinearOperatorMatrix(
-                    math_ops.cast(v.to_dense(), dtype))
-            else:
-                v = ops.convert_to_tensor(v, dtype=dtype)
-            retval[k] = v
-        return retval
-
-    def _verify(self, v, expected_x, **kwargs):
-        for dtype in self._dtypes_to_test:
-            with self.test_session() as sess:
-                kwargs_dtype = self._convert_inputs(dtype, kwargs)
-                f = self._prox_function(**kwargs_dtype)
-                x = sess.run(f(ops.convert_to_tensor(v, dtype=dtype)))
-                self.assertEqual(dtype, x.dtype)
-                self.assertAllClose(expected_x, x, atol=1e-5)
-
-class AbsoluteValueTest(ProxFunctionTest):
-    @property
-    def _prox_function(self):
-        return functions.AbsoluteValue
-
-    def testDefault(self):
-        self._verify([-3.,-1.,4.], [-2.,0.,3.], scale=1.)
-    def testScaleSymmetric(self):
-        self._verify([-3.,-1.,4.], [-1.,0.,2.], scale=2.)
-    def testScaleAsymmetric(self):
-        self._verify([-4.,-2.,-1.,2.,3.], [-1.,0.,0.,1.,2.], scale=(3.,1.))
-    def testScale2d(self):
-        self._verify([[-3,-1,4.], [-3,-1,4.]],
-                     [[0,0,3.], [-2,0,1.]],
-                     scale=([[3],[1]],[[1],[3]]))
-
-class LeastSquaresTest(ProxFunctionTest):
-    @property
-    def _prox_function(self):
-        return functions.LeastSquares
-
-    def testBasic1d(self):
-        self._verify(
-            [[1.],[2.]], [[-0.853448], [2.448276]],
-            A=linalg.LinearOperatorMatrix([[1.,2.],[3.,4.],[5.,6.]]),
-            b=[[7.],[8.],[9.]])
-    def testBasic2d(self):
-        self._verify(
-            [[1.,3.], [2., 4.]],
-            [[-0.853448, -0.629311], [2.448276, 2.310345]],
-            A=linalg.LinearOperatorMatrix([[1.,2.],[3.,4.],[5.,6.]]),
-            b=[[7.],[8.],[9.]])
-    def testBasic1dWithMu(self):
-        self._verify(
-            [[1.],[2.]], [[-0.728593], [2.347778]],
-            A=linalg.LinearOperatorMatrix([[1.,2.],[3.,4.],[5.,6.]]),
-            b=[[7.],[8.],[9.]],
-            mu=0.1)
+  # def np_solve(A, b, C, d, W, v, mu):
+  #   # [A'A + W'W + mu*I  C'][x] = [A'b + W'v]
+  #   # [C                 0 ][y]   [d]
+  #   I = np.eye(A.shape[1])
+  #   L1 = np.linalg.cholesky(A.T.dot(A) + W.T.dot(W) + mu*I)
+  #   F = np_cholesky_solve(L1, C.T)
+  #   L2 = C.dot(C.dot(F))
+  #   g = A.T.dot(b) + W.T.dot(v)
+  #   y = np_cholesky_solve(L2, F.T.dot(g) - d)
+  #   x = np_cholesky_solve(L1, g - C.T.dot(y))
+  #   return x
 
 if __name__ == "__main__":
-    test.main()
+  test.main()
