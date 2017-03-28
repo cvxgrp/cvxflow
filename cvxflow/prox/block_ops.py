bf5128130b259bfafb18bc9798832d61d052d854
Matt Wytock <matt@gridmatic.com>
2017-03-28 09:14:27 -0700

    add block ops and block separable

---
+++ cvxflow/prox/block_ops.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+
+def get_slices(shape):
+  offset = 0
+  slices = []
+  for shape_i in shape:
+    size_i = np.prod(shape_i)
+    slices.append((slice(offset, offset+size_i), shape_i))
+    offset += size_i
+  return slices, offset
+
+
+def to_list(x, slices, name="to_list"):
+  with ops.name_scope(name, values=[x]):
+    return [array_ops.reshape(x[idx], shape) for idx, shape in slices]
+
+def vec(x_list, name="vec"):
+  with ops.name_scope(name, values=x_list):
+    return array_ops.concat(
+      [array_ops.reshape(x, (-1, 1)) for x in x_list], axis=0)
---
+++ cvxflow/prox/block_separable.py
@@ -0,0 +1,18 @@
+
+from tensorflow.python.framework import ops
+
+from cvxflow.prox import prox_function
+from cvxflow.prox import block_ops
+
+
+class BlockSeparable(prox_function.ProxFunction):
+  def __init__(self, prox=None, shape=None, name="BlockSeparable"):
+    with ops.name_scope(name):
+      self.slices, _ = block_ops.get_slices(shape)
+      self.prox = prox
+      super(BlockSeparable, self).__init__(name=name)
+
+  def _call(self, v):
+    return block_ops.vec(
+      [prox_i(v_i) for prox_i, v_i
+       in zip(self.prox, block_ops.to_list(v, self.slices))])
